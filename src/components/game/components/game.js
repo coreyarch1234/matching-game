cdb536843e079b2959f1ce47eda8eec470c70c23
coreyarch1234 <harrilal@mit.edu>
2018-03-05 16:35:39 -0800

    can match and remove cards from board

--- src/components/game/components/game.js
+++ src/components/game/components/game.js
@@ -6,8 +6,32 @@ export default class Game extends Component {
     super(props);
     this.state = {
       cards: this.makeDeck(),
-      firstPick: null
+      firstPick: null,
+      matchedCards: []
     }
+  }
+
+  removeMatched(rank, suitOne, suitTwo) {
+    // hide those cards on board
+    //add to matched array
+    console.log(`remove matched`);
+    const newCards = this.state.cards.filter((cardObject, index) => {
+      const cardRank = cardObject.rank;
+      const cardSuit = cardObject.suit;
+      if (cardRank === rank && cardSuit === suitOne){
+        cardObject.className = 'hideCard';
+        console.log(`we are hiding card, ${rank} ${suitOne}`);
+        return cardObject;
+      }else if (cardRank === rank && cardSuit === suitTwo){
+        cardObject.className = 'hideCard';
+        console.log(`we are hiding card, ${rank} ${suitTwo}`);
+        return cardObject;
+      }
+      else{
+        return cardObject;
+      }
+    });
+    this.setState({cards: newCards, firstPick: null});
   }
   clickCard(rank, suit) {
     console.log(`first pick is now: ${this.state.firstPick === null ? this.state.firstPick : this.state.firstPick.rank}`);
@@ -19,12 +43,17 @@ export default class Game extends Component {
         suit: suit
       }});
     }else if (this.state.firstPick.rank === rank) {
+      // hide those cards and reset first pick
       console.log(`ranks are equal: ${rank}`);
+      this.removeMatched(rank, this.state.firstPick.suit, suit);
     }else{
+      // reset first pick
       console.log(`ranks are not equal: ${rank}`);
+      this.setState({firstPick: null});
     }
   }
   makeDeck() {
+    console.log(`WE ARE MAKING A DECK`);
     const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
     const suits = ['S', 'H', 'C', 'D'];
     const cards = []
@@ -44,7 +73,7 @@ export default class Game extends Component {
   }
   render() {
     console.log(`first pick is now: ${this.state.firstPick === null ? this.state.firstPick : this.state.firstPick.rank}`);
-    this.makeDeck();
+    // this.makeDeck();
     return (
       <div className="App">
         <Board cards={this.state.cards} onClick={(rank, suit) => this.clickCard(rank, suit)}/>
